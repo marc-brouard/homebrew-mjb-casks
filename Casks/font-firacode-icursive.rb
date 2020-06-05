eea06060205991f2e98c2648918db3ed275206d7
marc-brouard <marc.brouard@zoo.ox.ac.uk>
2020-06-05 14:28:55 +0100

    Update font-firacode-icursive.rb

--- Casks/font-firacode-icursive.rb
+++ Casks/font-firacode-icursive.rb
@@ -2,7 +2,7 @@ cask 'font-firacode-icursive' do
   version '1.0.0'
   sha256 :no_check
   
-  url "https://github.com/marc-brouard/homebrew-mjb-casks/releases/download/#{version]/Archive.zip"
+  url "https://github.com/marc-brouard/homebrew-mjb-casks/releases/download/#{version}/Archive.zip"
   name 'Fira Code iCursive S12'
   homepage 'https://www.sainnhe.dev/post/icursive-nerd-font-preview/'
   
