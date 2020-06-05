606c2a4dade648a5e018fadd700abd9a04ecd843
marc-brouard <marc.brouard@zoo.ox.ac.uk>
2020-06-05 13:00:55 +0100

    Update font-firacode-icursive.rb

--- Casks/font-firacode-icursive.rb
+++ Casks/font-firacode-icursive.rb
@@ -4,7 +4,7 @@ cask 'font-firacode-icursive' do
   
   url 'https://github.com/sainnhe/icursive-nerd-font/tree/master/Fira Code iCursive S12'
   name 'Fira Code iCursive S12'
-  homepage 'https://www.sainnhe.dev/post/icursive-nerd-font-preview/#fira-code-icursive-op-non-free'
+  homepage 'https://www.sainnhe.dev/post/icursive-nerd-font-preview/'
   
   font 'Fira Code iCursive S12 Bold.ttf'
   font 'Fira Code iCursive S12 Italic.ttf'
